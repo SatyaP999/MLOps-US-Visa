20d3781a124e1ddca06a986cd04b6b118895a809
SatyaP999 <satya.pola45@gmail.com>
2024-09-12 04:46:46 +0530

    Data Ingestion added

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
-from us_visa.logger import logging
-from us_visa.exception import USVisaException
-import sys
+# import os
 
-logging.info("Welcome to custom log.")
+# mongo_db_url = os.getenv('MONGODB_URL')
+# print(mongo_db_url)
 
-try:
-    a = 2/0
-except Exception as e:
-    raise USVisaException(e, sys)
+from us_visa.pipeline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+
+obj.run_pipeline()
