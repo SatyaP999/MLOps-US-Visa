d592aa9f6ad89ed0e81372f4e1a2073a9c35c4ac
SatyaP999 <satya.pola45@gmail.com>
2024-09-12 07:37:04 +0530

    data validation added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -17,6 +17,12 @@ TEST_FILE_NAME: str = "test.csv"
 
 MODEL_FILE_NAME = "model.pkl"
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = datetime.today().year
+PREPROCESSING_OBJECR_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+
 '''
 Data Ingestion related constants starts with DATA_INGESTION name
 '''
@@ -26,3 +32,12 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+'''
+Data Validation related constants starts with DATA_INGESTION name
+'''
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
